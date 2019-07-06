188ed2e05bedbc4372f39ddca6ea6046b062852d
Emmett Keyser <emkeyser@gmail.com>
2019-07-06 08:20:52 -0700

    wip

--- bake.sh
+++ bake.sh
@@ -3,23 +3,32 @@
 
 # password specified on cmd line?
 # prompt for password?
-PASS=$1
+echo "Please supply password for administrator@corp.helm.ai"
+read -s ADMINPASS
+echo "ADMINPASS is '${ADMINPASS}'"
+echo "Please supply fully capitalized FQDN (CORP.EXAMPLE.COM)"
+read FQDN
+echo "Please supply fully capitalized Kerberos realm (EXAMPLECOM)"
+read SHORTNAME
+exit
+
+
 export DEBIAN_FRONTEND=noninteractive
 
 sudo apt update
 sudo apt -yq install krb5-user samba sssd chrony
-
+exit
 # apply replacement values
-./mix.sh
+#./mix.sh
 
 # patch /etc/krb5.conf
 # patch /etc/samba/smb.conf
 # patch /etc/pam.d/common-session
-sudo cp sssd.conf /etc/sssd/
-sudo chown root:root /etc/sssd/sssd.conf
-sudo chmod 600 /etc/sssd/sssd.conf
-sudo systemctl restart smbd.service nmbd.service chrony.service
-sudo kinit Administrator ${PASS}
-sudo systemctl restart smbd.service nmbd.service chrony.service sssd.service
+cp sssd.conf /etc/sssd/
+chown root:root /etc/sssd/sssd.conf
+chmod 600 /etc/sssd/sssd.conf
+systemctl restart smbd.service nmbd.service chrony.service
+kinit Administrator ${PASS}
+systemctl restart smbd.service nmbd.service chrony.service sssd.service
 
 
